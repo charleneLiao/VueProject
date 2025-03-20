0d11ed8a12db8c198943e0949d325a449d74d13f
Giselle <louisliao696@gmail.com>
2025-03-20 14:20:32 +0800

    test

--- all02.js
+++ all02.js
@@ -3,6 +3,7 @@ const app = Vue.createApp({
         return{
             name: ["同學","小美"],
             link: "https://github.com/charleneLiao",
+            todos: ["廁所清潔","洗手台清潔","補充衛生紙"],
         };
     },
 });
